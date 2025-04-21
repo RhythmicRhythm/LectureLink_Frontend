685338eabd8c23af05297503bbe34b1e46fa4235
RhythmicRhythm <okubadejorh@gmail.com>
2025-04-21 18:59:34 +0100

    Remove unused imports and commented-out code in Dashboard component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from "react";
-
 import Sidebar from "../partials/Sidebar";
 import Header from "../partials/Header";
-import DashboardCard07 from "../partials/dashboard/DashboardCard07";
-// import Info from "../partials/dashboard/Info";
 import useRedirectLoggedOutUser from "../customHook/useRedirectLoggedOutUser";
 import WelcomeSection from "../sections/WelcomeSection";
 import CoursesSection from "../sections/CoursesSection";
@@ -27,8 +24,7 @@ function Dashboard() {
           <div className="px-2 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
             {/* Cards */}
             <div className="">
-              {/* Table (Top Channels) */}
-              {/* <Info /> */}
+      
               <div className="grid grid-cols-12 gap-10">
                 
               </div>
