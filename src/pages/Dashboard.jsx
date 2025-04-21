a574cb38e73dc9a812d93648eb0841f8354f91ba
RhythmicRhythm <okubadejorh@gmail.com>
2025-04-21 18:03:30 +0100

    Remove unused  components

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -14,12 +14,10 @@ function Dashboard() {
     <div className="flex h-screen overflow-hidden">
       {/* Sidebar */}
       <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-
       {/* Content area */}
       <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
         {/*  Site header */}
         <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-
         <main>
           <div className="px-2 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
             {/* Cards */}
